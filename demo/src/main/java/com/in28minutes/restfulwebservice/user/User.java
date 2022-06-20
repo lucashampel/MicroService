8c989b238f8d8747846df7856440d901a93cf202
lucas <lucas@LAPTOP-M5IEOFO5>
2022-06-20 11:17:00 -0400

    First commit with all base files

---
+++ demo/src/main/java/com/in28minutes/restfulwebservice/user/User.java
@@ -0,0 +1,57 @@
+package com.in28minutes.restfulwebservice.user;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+
+	private Integer id;
+	private String name;
+	private Date dateOfBirth;
+	
+	public User(Integer id, String name, Date dateOfBirth) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", dateOfBirth=" + dateOfBirth + "]";
+	}
+	
+	
+
+	
+
+}
---
+++ demo/src/main/java/com/in28minutes/restfulwebservice/user/UserResource.java
@@ -0,0 +1,8 @@
+package com.in28minutes.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+}
